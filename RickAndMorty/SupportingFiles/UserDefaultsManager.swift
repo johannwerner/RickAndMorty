a8c39960ab38451b904bdeae4e56cdbc9aab8bd1
johannwerner <johannwerner1860@gmail.com>
2020-01-31 16:23:49 +0200

    Using property wrappers for user defaults

--- RickAndMorty/SupportingFiles/UserDefaultsManager.swift
+++ RickAndMorty/SupportingFiles/UserDefaultsManager.swift
@@ -12,29 +12,46 @@ import UIKit
 
 class UserDefaultsManager {
     // MARK: - Properties
-    
+    @UserDefaultsProperty(userDefaultsKey: UserDefaultConstants.favoriteIds, initalValue: [Int]())
+    var listOfFavoriteIds
 
     // MARK: - Life Cycle
 
 }
 
-extension UserDefaultsManager {
-    var listOfFavoriteIds: [Int] {
-        get {
-            let defaults = UserDefaults.standard
-            let object = defaults.object(forKey: UserDefaultConstants.favoriteIds)
-            guard let characterIds = object as? [Int] else {
-                return []
-            }
-            return characterIds
-        }
-        set {
-            let defaults = UserDefaults.standard
-            defaults.set(newValue, forKey: UserDefaultConstants.favoriteIds)
-        }
-    }
-}
-
 private struct UserDefaultConstants {
     static let favoriteIds = "FavoriteIds"
+}
+
+@propertyWrapper
+struct UserDefaultsProperty<X: Codable> {
+    private let userDefaultsKey: String
+    private let initalValue: X
+
+    init(userDefaultsKey: String, initalValue: X) {
+        self.userDefaultsKey = userDefaultsKey
+        self.initalValue = initalValue
+    }
+
+    var wrappedValue: X {
+        get {
+            guard let data = UserDefaults.standard.object(forKey: userDefaultsKey) as? Data else {
+                if let value = UserDefaults.standard.object(forKey: userDefaultsKey) as? X {
+                    return value
+                }
+                return initalValue
+            }
+            guard let value = try? JSONDecoder().decode(X.self, from: data) else {
+                return initalValue
+            }
+            return value
+        }
+        set {
+            guard let data = try? JSONEncoder().encode(newValue) else {
+                UserDefaults.standard.set(newValue, forKey: userDefaultsKey)
+                return
+            }
+            UserDefaults.standard.set(data, forKey: userDefaultsKey)
+        }
+    }
 }
